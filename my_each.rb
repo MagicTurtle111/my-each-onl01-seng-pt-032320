b196f99593ca8f5a08010e278039d07feea3d051
Joshua Williams <j.o-williams@outlook.com>
2020-04-05 13:35:55 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  i = 0
+
+  while i < array.size
+    #isolate a single element of the array
+    #yield the element taht we have isolated  
+  end    
+  
 end
